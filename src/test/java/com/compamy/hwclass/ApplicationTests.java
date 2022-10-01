e8153384dd803755558dcff261a638a742cb0dae
AleksandrVavilov <vavilov3388@gmail.com>
2022-10-01 16:39:12 +0300

    One commit

---
+++ src/test/java/com/compamy/hwclass/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compamy.hwclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
